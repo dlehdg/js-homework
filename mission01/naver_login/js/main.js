1c1b324ad06f291ab934dc65d4ca118d7ceb83ed
dlehdg <ldg2409@naver.com>
2023-12-09 22:48:30 +0900

    요구사항 2번 완료

--- mission01/naver_login/js/main.js
+++ mission01/naver_login/js/main.js
@@ -1,8 +1,7 @@
-
 const user = {
-  id:'asd@naver.com',
-  pw:'spdlqj123!@'
-}
+  id: "asd@naver.com",
+  pw: "spdlqj123!@",
+};
 
 /*
 
@@ -13,24 +12,43 @@ const user = {
 
 */
 
-function emailReg(text){
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const email = document.querySelector(".user-email-input");
 
-  return re.test(String(text).toLowerCase())
+const password = document.querySelector(".user-password");
+
+function emailReg(text) {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  return re.test(String(text).toLowerCase());
 }
 
-function pwReg(text){
+function pwReg(text) {
   const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{6,16}$/;
   return re.test(String(text).toLowerCase());
 }
 
+email.addEventListener("input", (e) => {
+  // if (e.target.value !== "") {
+  if (emailReg(e.target.value)) {
+    console.log(emailReg(e.target.value) === true ? "참" : "거짓");
+    email.classList.remove("is--invalid");
+  }
+  // (!pwReg(e.target.value))
+  else if (!emailReg(e.target.value)) {
+    console.log(emailReg(e.target.value));
+    email.classList.add("is--invalid");
+  }
+});
 
-
-
-
-
-
-
-
-
-
+password.addEventListener("input", (e) => {
+  if (pwReg(e.target.value)) {
+    console.log(pwReg(e.target.value));
+    password.classList.remove("is--invalid");
+  }
+  // (!pwReg(e.target.value))
+  else {
+    console.log(pwReg(e.target.value));
+    password.classList.add("is--invalid");
+  }
+});
